3349eef993576e73dcbfd6c143e263cde3f11312
theboringspace <214440631+theboringspace@users.noreply.github.com>
2026-09-19 15:34:49 -0700

    Document include/ and src/

--- build/CMakeFiles/PingPlusPlus.dir/src/Paddle.cpp.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/PingPlusPlus.dir/src/Paddle.cpp.o: \
- /home/theboringspace/Desktop/PongPlusPlus/src/Paddle.cpp \
- /usr/include/stdc-predef.h \
- /home/theboringspace/Desktop/PongPlusPlus/include/Paddle.hpp \
- /usr/local/include/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/15/include/stdarg.h
