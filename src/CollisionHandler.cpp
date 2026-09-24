6d29d916279322499b0e3f6ec1c81bf68f373f8e
theboringspace <214440631+theboringspace@users.noreply.github.com>
2026-09-23 23:03:10 -0700

    Fix CollisionHandler. Multiple .at()'s -> const Paddle& currentPaddle

--- src/CollisionHandler.cpp
+++ src/CollisionHandler.cpp
@@ -9,8 +9,8 @@ void CollisionHandler::ResolveCollisions(Ball& ball, const std::array<Paddle, PA
     for (size_t index{0}; index < paddles.size(); ++index)
     {
         const Paddle& currentPaddle{paddles.at(index)};
-        currentPaddleRect = Rectangle{ paddles.at(index).GetPosition().x, paddles.at(index).GetPosition().y,
-                                       paddles.at(index).GetDimensions().x, paddles.at(index).GetDimensions().y, };
+        currentPaddleRect = Rectangle{ currentPaddle.GetPosition().x, currentPaddle.GetPosition().y,
+                                       currentPaddle.GetDimensions().x, currentPaddle.GetDimensions().y, };
 
        if (!CheckCollisionCircleRec(ball.center, ball.radius, currentPaddleRect)) { continue; }
 
@@ -18,13 +18,13 @@ void CollisionHandler::ResolveCollisions(Ball& ball, const std::array<Paddle, PA
         if (index == LEFT)
         {
             ball.velocity.x = std::abs(ball.velocity.x);
-            ball.center.x   = paddles.at(index).GetPosition().x + paddles.at(index).GetDimensions().x + ball.radius;
+            ball.center.x   = currentPaddle.GetPosition().x + currentPaddle.GetDimensions().x + ball.radius;
         }
         // Hits right paddle
         else
         {
             ball.velocity.x = -std::abs(ball.velocity.x);
-            ball.center.x   = paddles.at(index).GetPosition().x - ball.radius;
+            ball.center.x   = currentPaddle.GetPosition().x - ball.radius;
         }
     }
 }
